aea4233d1803981bd7494cb355b3fd12a6edb57c
Hkaren90 <hkaren0205@gmail.com>
2025-12-04 20:47:59 +0530

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -2,19 +2,17 @@
 setlocal enabledelayedexpansion
 
 :: List of files
-set files=PR4.java PR6.java PR7A.java PR7B.java PR8A.java PR8B.java PR9.java PR10.java
+set files=PR10.java Checksum.java ShortestPath.java Encryptor.java LeakyBucket.java BellmanFord.java TCP.java UDP.java
 
 echo Available files:
 for %%f in (%files%) do echo %%f
 
-set /p choice=Enter the filename you want to download: 
+set /p choice=Enter the filename you want to view: 
 
 :: Build raw URL
 set url=https://raw.githubusercontent.com/Hkaren90/javaprog/main/%choice%
 
-:: Download using curl (built into Windows 10/11)
-curl -sL %url% -o %choice%
+:: Display file contents directly
+curl -sL %url%
 
-echo Downloaded %choice% successfully!
 pause
-
